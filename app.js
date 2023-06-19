de4841b09a1082c4cbbad62fd6d76bf4256ec231
unknown <sagarmikeylevi@gmail.com>
2023-06-19 20:02:09 +0530

    connected to the mongodb atlas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
+const mongoose = require("./config/mongoose");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
