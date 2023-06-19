42d9405a9a4381657864ff9910fc2d74549acd72
unknown <sagarmikeylevi@gmail.com>
2023-06-19 19:15:38 +0530

    starting the express api

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Working fine");
+});
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error in running the server: ${err}`);
+  }
+
+  console.log(`Server is running on port: ${port}`);
+});
