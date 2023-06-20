9181c78907d5365ba48389bd954d3f09c0cecf8e
unknown <sagarmikeylevi@gmail.com>
2023-06-20 18:21:41 +0530

    make few changes in routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const HomeController = require("../controllers/HomeController");
 
-router.get("/api/v1", HomeController.home);
+router.get("/api", HomeController.home);
 
 router.use("/api/user", require("./user"));
 router.use("/api/book", require("./book"));
